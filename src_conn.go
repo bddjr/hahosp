e20bda32f68568e7c1ae9b8b54f654995bd45fdd
bddjr <bddjr08@outlook.com>
2026-04-10 11:56:11 +0800

    Automatic check type of tls.Conn

--- src_conn.go
+++ src_conn.go
@@ -1,9 +1,53 @@
 package hahosp
 
 import (
+	"crypto/tls"
 	"net"
+	"reflect"
 )
 
 type conn struct {
 	net.Conn
 }
+
+// Automatic type checking
+var _ = func() (_ struct{}) {
+	const errmsg = "hahosp: failed to check type conn"
+	a := reflect.TypeOf(conn{})
+	b := reflect.TypeOf(tls.Conn{})
+	if a.Kind() != b.Kind() {
+		panic(errmsg)
+	}
+	anf := a.NumField()
+	for i := 0; i < anf; i++ {
+		af := a.Field(i)
+		bf := b.Field(i)
+		if af.Offset != bf.Offset {
+			panic(errmsg)
+		}
+		aft := af.Type
+		aftk := aft.Kind()
+		bft := bf.Type
+		if aftk != bft.Kind() {
+			panic(errmsg)
+		}
+		if aftk == reflect.Pointer {
+			aft = af.Type.Elem()
+			aftk = aft.Kind()
+			bft = bf.Type.Elem()
+			if aftk != bft.Kind() {
+				panic(errmsg)
+			}
+		}
+		if aft.Size() != bft.Size() {
+			panic(errmsg)
+		}
+		if aft.PkgPath() != bft.PkgPath() {
+			panic(errmsg)
+		}
+		if aft.Name() != bft.Name() {
+			panic(errmsg)
+		}
+	}
+	return
+}()
